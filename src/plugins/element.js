5ccc1e4a04d21aa67d35f463f553adcc045e5744
qjp <qiujianp123@163.com>
2020-04-23 18:00:49 +0800

    登录功能1

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input } from 'element-ui'
+// 导入消息提示弹框组件
+import { Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 挂载到 vue 中
+Vue.prototype.$message = Message
