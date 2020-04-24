c89588d83363a5591b2d0f29f63dbd9c8606ec13
qjp <qiujianp123@163.com>
2020-04-24 12:29:32 +0800

    修改element-ui的按需导入

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
-// 导入消息提示弹框组件
-import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-// 挂载到 vue 中
+// 挂载到 vue 中 导入消息提示弹框组件
 Vue.prototype.$message = Message
