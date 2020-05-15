1be05a101434863cad35bac6178784c1d5fad946
qjp <qiujianp123@163.com>
2020-05-15 17:45:25 +0800

    首页内容定制化

--- vue.config.js
+++ vue.config.js
@@ -17,9 +17,17 @@ module.exports = {
         nprogress: 'NProgress',
         'vue-quill-editor': 'VueQuillEditor'
       })
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
     })
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
     })
   }
 }
