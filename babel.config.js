e7a10ed558c5bbca905aa30693b49d9d13085856
qjp <qiujianp123@163.com>
2020-06-07 17:49:02 +0800

    增加一些注释

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 // 这是项目发布阶段需要用到的 babel 插件
 const prodPlugins = []
 if (process.env.NODE_ENV === 'production') {
+  // 判断为线上模式，去除console有关的js代码
   prodPlugins.push('transform-remove-console')
 }
 
@@ -18,6 +19,7 @@ module.exports = {
     ],
     // 发布产品时候的插件数组
     ...prodPlugins,
+    // 路由懒加载 babel 插件
     '@babel/plugin-syntax-dynamic-import'
   ]
 }
--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     open: true
   },
   chainWebpack: config => {
+    // 打包为线上触发将资源引入为外部 CDN
     config.when(process.env.NODE_ENV === 'production', config => {
       config.entry('app').clear().add('./src/main-prod.js')
       // 通过external加载外部 CDN 资源，由于cdn无最新版的 vue-router 3.1.6 暂时注释
